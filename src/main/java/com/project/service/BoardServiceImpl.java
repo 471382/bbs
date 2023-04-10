94d783dc0b7004076941c1dea542b037ae1b32f7
471382 <woofer97@naver.com>
2023-04-10 18:06:40 +0900

    root-context에 mybatis 설정파일 기술 및 Service인터페이스,구현부 작성

---
+++ src/main/java/com/project/service/BoardServiceImpl.java
@@ -0,0 +1,54 @@
+package com.project.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.dao.BoardDao;
+import com.project.dto.BoardDto;
+import com.project.vo.PageMaker;
+
+@Service
+public class BoardServiceImpl implements IBoardService {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void write(BoardDto board) throws Exception {
+		System.out.println(sqlSession);
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		dao.create(board);
+	}
+
+	@Override
+	public BoardDto read(int bno) throws Exception {
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		return dao.read(bno);
+	}
+
+	@Override
+	public void modify(BoardDto board) throws Exception {
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		dao.update(board);
+	}
+
+	@Override
+	public void remove(int bno) throws Exception {
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		dao.delete(bno);
+	}
+
+	@Override
+	public List<BoardDto> listSearchCriteria(PageMaker pm) throws Exception {
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		return dao.listSearch(pm);
+	}
+
+	@Override
+	public int listSearchCount(PageMaker pm) throws Exception {
+		BoardDao dao=sqlSession.getMapper(BoardDao.class);
+		return dao.listSearchCount(pm);
+	}
+}
---
+++ src/main/java/com/project/service/IBoardService.java
@@ -0,0 +1,16 @@
+package com.project.service;
+
+import java.util.List;
+
+import com.project.dto.BoardDto;
+import com.project.vo.PageMaker;
+
+public interface IBoardService {
+	public void write(BoardDto board) throws Exception;
+	public BoardDto read(int bno) throws Exception;
+	public void modify(BoardDto board) throws Exception;
+	public void remove(int bno) throws Exception;
+	
+	public List<BoardDto> listSearchCriteria(PageMaker pm) throws Exception;
+	public int listSearchCount(PageMaker pm) throws Exception;
+}
