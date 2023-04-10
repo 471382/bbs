bab944548f79994bfff8a4d1b82796fde3112d38
471382 <woofer97@naver.com>
2023-04-10 18:01:25 +0900

    Dto작성

---
+++ src/main/java/com/project/dto/BoardDto.java
@@ -0,0 +1,52 @@
+package com.project.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class BoardDto {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
+	private LocalDateTime regdate;
+	private int viewcnt;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public LocalDateTime getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(LocalDateTime regdate) {
+		this.regdate = regdate;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+	
+}
